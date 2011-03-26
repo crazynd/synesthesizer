8556de7ec91a9851c0716d6b90ddb42f9cb0adff
Andreas Lappe <nd@off-pist.de>
2011-03-26 08:32:24 +0100

    Initial Commit

---
+++ synesthesizer/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  synesthesizer
+//
+//  Created by nd on 26.03.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ synesthesizerTests/synesthesizerTests.m
@@ -0,0 +1,33 @@
+//
+//  synesthesizerTests.m
+//  synesthesizerTests
+//
+//  Created by nd on 26.03.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "synesthesizerTests.h"
+
+
+@implementation synesthesizerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in synesthesizerTests");
+}
+
+@end
